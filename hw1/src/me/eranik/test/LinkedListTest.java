b148e33970c1d2147a83dcf73e66051f296341b7
AntonYermilov <anton.yermilov@gmail.com>
2017-09-20 21:02:05 +0300

    added tests for linked list

--- hw1/src/me/eranik/test/LinkedListTest.java
+++ hw1/src/me/eranik/test/LinkedListTest.java
@@ -28,15 +28,14 @@ public class LinkedListTest {
 
     @Test
     public void get() {
-        assertEquals(list.get(-1), null);
         assertEquals(list.get(0), null);
         list.add("1");
         list.add("2");
         list.add("3");
         assertEquals(list.get(0), "1");
-        assertEquals(list.get(2), "2");
-        assertEquals(list.get(3), "3");
-        assertEquals(list.get(4), null);
+        assertEquals(list.get(1), "2");
+        assertEquals(list.get(2), "3");
+        assertEquals(list.get(3), null);
     }
 
     @Test
@@ -44,7 +43,7 @@ public class LinkedListTest {
         for (int i = 0; i < 5; i++) {
             list.add(i);
         }
-        for (int i = -1; i < 6; i++) {
+        for (int i = 0; i < 5; i++) {
             list.set(i, 10 + i);
         }
         for (int i = 0; i < 5; i++) {
@@ -54,31 +53,93 @@ public class LinkedListTest {
 
     @Test
     public void remove() {
+        for (int i = 0; i < 10; i++) {
+            list.add(i);
+        }
+        for (int i = 0; i < 5; i++) {
+            list.remove(i + 1);
+        }
+
+        assertEquals(list.size(), 5);
+        for (int i = 0; i < 5; i++) {
+            assertEquals(list.get(i), 2 * i);
+        }
     }
 
     @Test
     public void indexOf() {
+        for (int i = 0; i < 10; i++) {
+            list.add(i);
+        }
+        for (int i = 0; i < 10; i++) {
+            list.add(2 * i);
+        }
+
+        for (int i = 0; i < 10; i++) {
+            assertEquals(list.indexOf(i), i);
+        }
+        for (int i = 10; i < 20; i += 2) {
+            assertEquals(list.indexOf(i), 10 + i / 2);
+        }
+        for (int i = 11; i < 20; i += 2) {
+            assertEquals(list.indexOf(i), -1);
+        }
     }
 
     @Test
     public void contains() {
+        for (int i = 0; i < 10; i++) {
+            list.add(2 * i);
+        }
+        for (int i = 0; i < 10; i++) {
+            assertTrue(list.contains(2 * i));
+            assertFalse(list.contains(2 * i + 1));
+        }
     }
 
     @Test
     public void isEmpty() {
-
+        assertTrue(list.isEmpty());
+        list.add(1);
+        assertFalse(list.isEmpty());
+        list.remove(0);
+        assertTrue(list.isEmpty());
     }
 
     @Test
     public void size() {
+        assertEquals(list.size(), 0);
+        for (int i = 0; i < 500; i++) {
+            list.add(i);
+            assertEquals(list.size(), i + 1);
+        }
+        for (int i = 0; i < 250; i++) {
+            list.remove(0);
+            assertEquals(list.size(), 499 - i);
+        }
+        list.clear();
+        assertEquals(list.size(), 0);
     }
 
     @Test
     public void clear() {
+        for (int i = 0; i < 10; i++) {
+            list.add("abc");
+        }
+        list.clear();
+        assertTrue(list.isEmpty());
     }
 
     @Test
     public void toArray() {
+        for (int i = 0; i < 10; i++) {
+            list.add(i);
+        }
+
+        Object[] array = list.toArray();
+        for (int i = 0; i < 10; i++) {
+            assertEquals((int)(array[i]), i);
+        }
     }
 
 }
