96b33ac35ed3d24f44ac67a9a21d2eb48e657e15
Anton Yermilov <anton.yermilov@gmail.com>
2017-09-28 02:53:45 +0300

    fixed camel case in tests

--- hw2-spiral/src/test/java/me/eranik/util/MatrixTest.java
+++ hw2-spiral/src/test/java/me/eranik/util/MatrixTest.java
@@ -19,55 +19,55 @@ public class MatrixTest {
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void matrixConstructor_NullMatrix() {
+    public void testMatrixConstructorNullMatrix() {
         new Matrix(null);
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void matrixConstructor_InvalidDimensions_1() {
+    public void testMatrixConstructorInvalidDimensionsEvenSize() {
         new Matrix(new int[][]{{1, 2}, {3, 4}});
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void matrixConstructor_InvalidDimensions_2() {
+    public void testMatrixConstructorInvalidDimensionsDifferentRowSize() {
         new Matrix(new int[][]{{1, 2, 3}, {4, 5}, {7, 8, 9}});
     }
 
     @Test
-    public void matrixConstructor_ValidDimensions() throws Exception {
+    public void testMatrixConstructorValidDimensions() throws Exception {
         new Matrix(new int[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}});
     }
 
     @Test
-    public void drawSpiral_1() throws Exception {
+    public void testDrawSpiralSmall() throws Exception {
         Matrix matrix =  new Matrix(new int[][]{{1}});
         matrix.drawSpiral();
         assertEquals("1", output.toString().trim());
     }
 
     @Test
-    public void drawSpiral_2() throws Exception {
+    public void testDrawSpiralBig() throws Exception {
         Matrix matrix =  new Matrix(new int[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}});
         matrix.drawSpiral();
         assertEquals("5 4 7 8 9 6 3 2 1", output.toString().trim());
     }
 
     @Test
-    public void printMatrix_1() throws Exception {
+    public void testPrintMatrixSmall() throws Exception {
         Matrix matrix = new Matrix(new int[][]{{1}});
         matrix.printMatrix();
         assertEquals("1", output.toString().trim());
     }
 
     @Test
-    public void printMatrix_2() throws Exception {
+    public void testPrintMatrixBig() throws Exception {
         Matrix matrix = new Matrix(new int[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}});
         matrix.printMatrix();
         assertEquals("1 2 3\n4 5 6\n7 8 9", output.toString().trim());
     }
 
     @Test
-    public void sortColumns_1() throws Exception {
+    public void testSrtColumnsSmall() throws Exception {
         Matrix matrix = new Matrix(new int[][]{{1}});
         matrix.sortColumns();
         matrix.printMatrix();
@@ -75,7 +75,7 @@ public class MatrixTest {
     }
 
     @Test
-    public void sortColumns_2() throws Exception {
+    public void testSortColumnsBig() throws Exception {
         Matrix matrix = new Matrix(new int[][]{{2, 1, 3}, {4, 5, 6}, {9, 7, 8}});
         matrix.sortColumns();
         matrix.printMatrix();
