1501172d87ce6a4f9854e63909366ba4dd16f5e9
Anton Yermilov <anton.yermilov@gmail.com>
2018-02-18 11:16:28 +0300

    hw1-pool: init commit, added thread pool and tests to it

---
+++ term2/hw1-pool/src/main/java/me/eranik/threads/LightFuture.java
@@ -0,0 +1,41 @@
+package me.eranik.threads;
+
+import java.util.function.Function;
+
+/**
+ * Special class that stores tasks and provides interaction with thread pool.
+ * @param <T> type of result that task returns
+ */
+public interface LightFuture<T> {
+
+    /**
+     * Checks if task is already processed or not
+     * @return {@code true} if task is already processed by thread pool; {@code false} otherwise
+     */
+    boolean isReady();
+
+    /**
+     * Waits until task processing is completed and returns its result.
+     * @return task result
+     * @throws LightExecutionException if exception occurred during task processing
+     */
+    T get() throws LightExecutionException;
+
+
+    /**
+     * Applies function to the result of task.
+     * @param function specified function
+     * @return result of application of specified function to the result of current task
+     */
+    LightFuture<T> thenApply(Function<T, T> function);
+
+    /**
+     * Exception that is thrown if some error occurred when executing task.
+     */
+    class LightExecutionException extends RuntimeException {
+        LightExecutionException(Exception e) {
+            super(e);
+        }
+    }
+
+}
