d8dd0e54a22fa12ecc9a927a31d702f28d04bbe8
Anton Yermilov <anton.yermilov@gmail.com>
2018-04-13 18:33:01 +0300

    cw1-md5 Updated tests

--- term2/cw1-md5/src/test/java/me/eranik/md5/MD5CalculatorTest.java
+++ term2/cw1-md5/src/test/java/me/eranik/md5/MD5CalculatorTest.java
@@ -13,18 +13,16 @@ import static org.junit.jupiter.api.Assertions.*;
  */
 class MD5CalculatorTest {
 
-    private byte[] resDir = new byte[] {
-            115, 96, 7, -125, 45, 33, 103, -70, -86, -25, 99, -3, 58, 63, 60, -15
-
-    };
     private byte[] resResources = new byte[] {
             85, -75, 88, -57, -17, -126, 14, 110, 0, -27, -103, 59, -98, 85, -39, 59,
-            115, 96, 7, -125, 45, 33, 103, -70, -86, -25, 99, -3, 58, 63, 60, -15,
             5, -51, -38, 120, -101, 1, 27, -98, -31, -60, 34, -33, 26, -26, 103, 86,
             49, 121, 105, 17, 9, -80, -66, -70, -57, -54, -5, 122, 53, 103, 20, -123
     };
     private byte[] resHello = new byte[] {
             5, -51, -38, 120, -101, 1, 27, -98, -31, -60, 34, -33, 26, -26, 103, 86
+    };
+    private byte[] resHello2 = new byte[] {
+            49, 121, 105, 17, 9, -80, -66, -70, -57, -54, -5, 122, 53, 103, 20, -123
     };
 
     private MD5Calculator calculator;
@@ -50,15 +48,15 @@ class MD5CalculatorTest {
     }
 
     @Test
-    void testDir() {
-        byte[] result = calculator.countMD5(new File("src/test/resources/dir"));
-        assertArrayEquals(resDir, result);
+    void testHello2() {
+        byte[] result = calculator.countMD5(new File("src/test/resources/hello2"));
+        assertArrayEquals(resHello2, result);
     }
 
     @Test
-    void testDirMultithread() {
-        byte[] result = calculatorMultithread.countMD5(new File("src/test/resources/dir"));
-        assertArrayEquals(resDir, result);
+    void testHello2Multithread() {
+        byte[] result = calculatorMultithread.countMD5(new File("src/test/resources/hello2"));
+        assertArrayEquals(resHello2, result);
     }
 
     @Test
